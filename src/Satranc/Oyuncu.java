894ff0e34d22b822c90d8533aab4acf8dcb30aaf
Fatma Koç <fatmakoc94@gmail.com>
2014-12-09 17:15:56 +0200

    mikemmel

--- src/Satranc/Oyuncu.java
+++ src/Satranc/Oyuncu.java
@@ -22,27 +22,27 @@ public class Oyuncu {
 		
 		if(tahta.tahta[koordinat.getbasY()][koordinat.getbasX()]=='P' || tahta.tahta[koordinat.getbasY()][koordinat.getbasX()]=='p')
 		{
-			//tahta.yerlestir(new Piyon(),koord,kendi_rengi,yedigi_renk,tahta);
+			tahta.yerlestir(new Piyon(),koordinat,tahta);
 		}
 		else if(tahta.tahta[koordinat.getbasY()][koordinat.getbasX()]=='K' || tahta.tahta[koordinat.getbasY()][koordinat.getbasX()]=='k')
 		{
-			//tahta.yerlestir(new Kale(),koord,kendi_rengi,yedigi_renk,tahta);
+			tahta.yerlestir(new Kale(),koordinat,tahta);
 		}
 		else if(tahta.tahta[koordinat.getbasY()][koordinat.getbasX()]=='F' || tahta.tahta[koordinat.getbasY()][koordinat.getbasX()]=='f')
 		{
-			//tahta.yerlestir(new Fil(),koord,kendi_rengi,yedigi_renk,tahta);
+			tahta.yerlestir(new Fil(),koordinat,tahta);
 		}
 		else if(tahta.tahta[koordinat.getbasY()][koordinat.getbasX()]=='A' || tahta.tahta[koordinat.getbasY()][koordinat.getbasX()]=='a')
 		{
-			//tahta.yerlestir(new At(),koord,kendi_rengi,yedigi_renk,tahta);
+			tahta.yerlestir(new At(),koordinat,tahta);
 		}
 		else if(tahta.tahta[koordinat.getbasY()][koordinat.getbasX()]=='V' || tahta.tahta[koordinat.getbasY()][koordinat.getbasX()]=='v')
 		{
-			//tahta.yerlestir(new Vezir(),koord,kendi_rengi,yedigi_renk,tahta);
+			tahta.yerlestir(new Vezir(),koordinat,tahta);
 		}
 		else if(tahta.tahta[koordinat.getbasY()][koordinat.getbasX()]=='Þ' || tahta.tahta[koordinat.getbasY()][koordinat.getbasX()]=='þ')
 		{
-			//tahta.yerlestir(new Sah(),koord,kendi_rengi,yedigi_renk,tahta);
+			tahta.yerlestir(new Sah(),koordinat,tahta);
 		}
 		
 		
--- src/Satranc/Piyon.java
+++ src/Satranc/Piyon.java
@@ -2,9 +2,10 @@ package Satranc;
 
 public class Piyon extends Tas {
 
-	@Override
+
 	public void hamleYap(Koordinat koordinat,Tahta tahta) {
-		// TODO Auto-generated method stub
+		
+		
 		
 	}
 
