14ad9ce288de7b06d487384629053a7f3624c376
Fatma Koç <fatmakoc94@gmail.com>
2014-12-09 15:26:23 +0200

    ssds

--- src/Satranc/Tahta.java
+++ src/Satranc/Tahta.java
@@ -2,4 +2,19 @@ package Satranc;
 
 public class Tahta {
 
+	public char tahta[][]=new char[9][9];
+	
+	
+	public Tahta(){
+		
+		tahta[1][1]='K'; tahta[1][2]='A'; tahta[1][3]='F'; tahta[1][4]='V'; tahta[1][5]='Ş'; tahta[1][6]='F'; tahta[1][7]='A'; tahta[1][8]='K';
+		tahta[2][1]='P'; tahta[2][2]='P'; tahta[2][3]='P'; tahta[2][4]='P'; tahta[2][5]='P'; tahta[2][6]='P'; tahta[2][7]='P'; tahta[2][8]='P';		
+		
+		//for WhitePlayer
+
+		tahta[8][1]='k'; tahta[8][2]='a'; tahta[8][3]='f'; tahta[8][4]='v'; tahta[8][5]='ş'; tahta[8][6]='f'; tahta[8][7]='a'; tahta[8][8]='k';
+		tahta[7][1]='p'; tahta[7][2]='p'; tahta[7][3]='p'; tahta[7][4]='p'; tahta[7][5]='p'; tahta[7][6]='p'; tahta[7][7]='p'; tahta[7][8]='p';		
+		
+	}
+	
 }
