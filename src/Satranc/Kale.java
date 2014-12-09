5cad8ee691102496dd090fc21dddc34506c7fad4
Fatma Koç <fatmakoc94@gmail.com>
2014-12-09 18:51:42 +0200

    Kale de tamamdır

--- src/Satranc/Kale.java
+++ src/Satranc/Kale.java
@@ -2,10 +2,199 @@ package Satranc;
 
 public class Kale extends Tas{
 
-	@Override
+	
 	public void hamleYap(Koordinat koordinat,Tahta tahta) {
-		// TODO Auto-generated method stub
+	
+		int a=koordinat.getbasY();
+		int b=koordinat.getbasX();
+		boolean girdi=false; 
 		
+		if(tahta.tahta[koordinat.getbasY()][koordinat.getbasX()]=='k') //beyazýn kale taþý
+		{
+		
+		
+		//dikey yukarý
+		
+		      if(koordinat.getsonY() < koordinat.getbasY() && koordinat.getbasX()==koordinat.getsonX())
+		           {
+			            while(tahta.tahta[a-1][b]==' ' || Character.isLowerCase(tahta.tahta[a-1][b])!=true || (koordinat.getsonX()==b && koordinat.getsonY()==a-1))
+		                     {
+			 
+			                     if(koordinat.getsonX()==b && koordinat.getsonY()==a-1 && Character.isLowerCase(tahta.tahta[a-1][b])==false)
+			                          {
+				                         tahta.tahta[koordinat.getsonY()][koordinat.getsonX()]=tahta.tahta[koordinat.getbasY()][koordinat.getbasX()];
+        		                         tahta.tahta[koordinat.getbasY()][koordinat.getbasX()]=' ';
+        		                         girdi=true;
+        		                         break;
+			                          }
+		           	     a=a-1;
+		                     }
+		           }
+		      
+		        a=koordinat.getbasY();
+				b=koordinat.getbasX();
+				
+				//dikey aþaðý
+				
+				if(girdi!=true && koordinat.getsonY() > koordinat.getbasY() && koordinat.getbasX()==koordinat.getsonX())
+				   {
+				        while(tahta.tahta[a+1][b]==' ' || Character.isLowerCase(tahta.tahta[a+1][b])!=true || (koordinat.getsonX()==b && koordinat.getsonY()==a+1))
+				             {
+					   
+				                	if(koordinat.getsonX()==b && koordinat.getsonY()==a+1 && Character.isLowerCase(tahta.tahta[a+1][b])==false)
+					                    {
+						                   tahta.tahta[koordinat.getsonY()][koordinat.getsonX()]=tahta.tahta[koordinat.getbasY()][koordinat.getbasX()];
+		        		                   tahta.tahta[koordinat.getbasY()][koordinat.getbasX()]=' ';
+		        		                   girdi=true;
+		        		                   break;
+					                    }
+					
+				  	         a=a+1;
+				             }
+				  }
+		
+				a=koordinat.getbasY();
+				b=koordinat.getbasX();
+				
+				//yatay sola 
+				
+				if(girdi!=true && koordinat.getsonY()==koordinat.getbasY() && koordinat.getbasX() > koordinat.getsonX())
+				{
+				     while(tahta.tahta[a][b-1]==' ' || Character.isLowerCase(tahta.tahta[a][b-1])!=true ||  (koordinat.getsonX()==b-1 && koordinat.getsonY()==a))
+			           	{
+					 
+				         	if(koordinat.getsonX()==b-1 && koordinat.getsonY()==a && Character.isLowerCase(tahta.tahta[a][b-1])==false)
+					            {
+						            tahta.tahta[koordinat.getsonY()][koordinat.getsonX()]=tahta.tahta[koordinat.getbasY()][koordinat.getbasX()];
+		        		            tahta.tahta[koordinat.getbasY()][koordinat.getbasX()]=' ';
+		        		            girdi=true;
+		        		            break;
+					            }
+					        b=b-1;
+			        	}
+				}
+				
+				a=koordinat.getbasY();
+				b=koordinat.getbasX();
+				
+				//yatay saða 
+				
+				if(girdi!=true && koordinat.getsonY()==koordinat.getbasY() && koordinat.getbasX() < koordinat.getsonX())
+				{
+				    while(tahta.tahta[a][b+1]==' ' || Character.isLowerCase(tahta.tahta[a][b+1])!=true || (koordinat.getsonX()==b+1 && koordinat.getsonY()==a))
+				       {
+					 
+				            	if(koordinat.getsonX()==b+1 && koordinat.getsonY()==a && Character.isLowerCase(tahta.tahta[a+1][b])==false)
+					                  {
+						                   tahta.tahta[koordinat.getsonY()][koordinat.getsonX()]=tahta.tahta[koordinat.getbasY()][koordinat.getbasX()];
+		        		                   tahta.tahta[koordinat.getbasY()][koordinat.getbasX()]=' ';
+		        		                   girdi=true;
+		        		                   break;
+					                   }	
+					      b=b+1;
+					
+				       }
+				}
+			 tahta.yazdir(); System.out.println(); 	
+		}
+		
+		         
+	        
+	        //Siyah Kale içinnnnn.....
+		
+		 if(tahta.tahta[koordinat.getbasY()][koordinat.getbasX()]=='K') //siyah kale taþý
+			{
+			
+			
+			//dikey yukarý
+			
+			      if(koordinat.getsonY() < koordinat.getbasY() && koordinat.getbasX()==koordinat.getsonX())
+			           {
+				            while(tahta.tahta[a-1][b]==' ' || Character.isUpperCase(tahta.tahta[a-1][b])!=true || (koordinat.getsonX()==b && koordinat.getsonY()==a-1))
+			                     {
+				 
+				                     if(koordinat.getsonX()==b && koordinat.getsonY()==a-1 && Character.isUpperCase(tahta.tahta[a-1][b])==false)
+				                          {
+					                         tahta.tahta[koordinat.getsonY()][koordinat.getsonX()]=tahta.tahta[koordinat.getbasY()][koordinat.getbasX()];
+	        		                         tahta.tahta[koordinat.getbasY()][koordinat.getbasX()]=' ';
+	        		                         girdi=true;
+	        		                         break;
+				                          }
+			           	     a=a-1;
+			                     }
+			           }
+			      
+			        a=koordinat.getbasY();
+					b=koordinat.getbasX();
+					
+					//dikey aþaðý
+					
+					if(girdi!=true && koordinat.getsonY() > koordinat.getbasY() && koordinat.getbasX()==koordinat.getsonX())
+					   {
+					        while(tahta.tahta[a+1][b]==' ' || Character.isUpperCase(tahta.tahta[a+1][b])!=true || (koordinat.getsonX()==b && koordinat.getsonY()==a+1))
+					             {
+						   
+					                	if(koordinat.getsonX()==b && koordinat.getsonY()==a+1 && Character.isUpperCase(tahta.tahta[a+1][b])==false)
+						                    {
+							                   tahta.tahta[koordinat.getsonY()][koordinat.getsonX()]=tahta.tahta[koordinat.getbasY()][koordinat.getbasX()];
+			        		                   tahta.tahta[koordinat.getbasY()][koordinat.getbasX()]=' ';
+			        		                   girdi=true;
+			        		                   break;
+						                    }
+						
+					  	         a=a+1;
+					             }
+					  }
+			
+					a=koordinat.getbasY();
+					b=koordinat.getbasX();
+					
+					//yatay sola 
+					
+					if(girdi!=true && koordinat.getsonY()==koordinat.getbasY() && koordinat.getbasX() > koordinat.getsonX())
+					{
+					     while(tahta.tahta[a][b-1]==' ' || Character.isUpperCase(tahta.tahta[a][b-1])!=true ||  (koordinat.getsonX()==b-1 && koordinat.getsonY()==a))
+				           	{
+						 
+					         	if(koordinat.getsonX()==b-1 && koordinat.getsonY()==a && Character.isUpperCase(tahta.tahta[a][b-1])==false)
+						            {
+							            tahta.tahta[koordinat.getsonY()][koordinat.getsonX()]=tahta.tahta[koordinat.getbasY()][koordinat.getbasX()];
+			        		            tahta.tahta[koordinat.getbasY()][koordinat.getbasX()]=' ';
+			        		            girdi=true;
+			        		            break;
+						            }
+						        b=b-1;
+				        	}
+					}
+					
+					a=koordinat.getbasY();
+					b=koordinat.getbasX();
+					
+					//yatay saða 
+					
+					if(girdi!=true && koordinat.getsonY()==koordinat.getbasY() && koordinat.getbasX() < koordinat.getsonX())
+					{
+					    while(tahta.tahta[a][b+1]==' ' || Character.isUpperCase(tahta.tahta[a][b+1])!=true || (koordinat.getsonX()==b+1 && koordinat.getsonY()==a))
+					       {
+						 
+					            	if(koordinat.getsonX()==b+1 && koordinat.getsonY()==a && Character.isUpperCase(tahta.tahta[a+1][b])==false)
+						                  {
+							                   tahta.tahta[koordinat.getsonY()][koordinat.getsonX()]=tahta.tahta[koordinat.getbasY()][koordinat.getbasX()];
+			        		                   tahta.tahta[koordinat.getbasY()][koordinat.getbasX()]=' ';
+			        		                   girdi=true;
+			        		                   break;
+						                   }	
+						      b=b+1;
+						
+					       }
+					}
+				  tahta.yazdir();  System.out.println();	
+			}
+		 
+		  
+		 
+		 
+		 
 	}
 
 }
--- src/Satranc/Vezir.java
+++ src/Satranc/Vezir.java
@@ -2,9 +2,13 @@ package Satranc;
 
 public class Vezir extends Tas {
 
-	@Override
+	
 	public void hamleYap(Koordinat koordinat,Tahta tahta) {
-		// TODO Auto-generated method stub
+		
+		
+		
+		
+		
 		
 	}
 
