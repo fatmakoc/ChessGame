292df1e5c2739d8200146d2ac536c221ad9121bc
Fatma Koç <fatmakoc94@gmail.com>
2014-12-09 15:55:40 +0200

    okey

--- src/Satranc/Oyna.java
+++ src/Satranc/Oyna.java
@@ -5,6 +5,7 @@ public class Oyna {
 	public static void main(String[] args) {
 		
 		Tahta tahta =new Tahta();
+		Koordinat koordinat=new Koordinat();
 		tahta.yazdir();
 		
 		
--- src/Satranc/Oyuncu.java
+++ src/Satranc/Oyuncu.java
@@ -2,4 +2,57 @@ package Satranc;
 
 public class Oyuncu {
 
+	Koordinat koordinat=new Koordinat();
+	Tahta tahta=new Tahta();
+	
+	public void tahtayiAl(Tahta tahta)
+	{
+		this.tahta=tahta;
+	}
+	
+	
+	public void koordinatiAl(Koordinat koordinat)
+	{
+		this.koordinat=koordinat;
+	}
+	
+	
+	public void tahtayaGonder() 
+	{
+		
+		if(tahta.tahta[koordinat.getbasY()][koordinat.getbasX()]=='P' || tahta.tahta[koordinat.getbasY()][koordinat.getbasX()]=='p')
+		{
+			//tahta.yerlestir(new Piyon(),koord,kendi_rengi,yedigi_renk,tahta);
+		}
+		else if(tahta.tahta[koordinat.getbasY()][koordinat.getbasX()]=='K' || tahta.tahta[koordinat.getbasY()][koordinat.getbasX()]=='k')
+		{
+			//tahta.yerlestir(new Kale(),koord,kendi_rengi,yedigi_renk,tahta);
+		}
+		else if(tahta.tahta[koordinat.getbasY()][koordinat.getbasX()]=='F' || tahta.tahta[koordinat.getbasY()][koordinat.getbasX()]=='f')
+		{
+			//tahta.yerlestir(new Fil(),koord,kendi_rengi,yedigi_renk,tahta);
+		}
+		else if(tahta.tahta[koordinat.getbasY()][koordinat.getbasX()]=='A' || tahta.tahta[koordinat.getbasY()][koordinat.getbasX()]=='a')
+		{
+			//tahta.yerlestir(new At(),koord,kendi_rengi,yedigi_renk,tahta);
+		}
+		else if(tahta.tahta[koordinat.getbasY()][koordinat.getbasX()]=='V' || tahta.tahta[koordinat.getbasY()][koordinat.getbasX()]=='v')
+		{
+			//tahta.yerlestir(new Vezir(),koord,kendi_rengi,yedigi_renk,tahta);
+		}
+		else if(tahta.tahta[koordinat.getbasY()][koordinat.getbasX()]=='Þ' || tahta.tahta[koordinat.getbasY()][koordinat.getbasX()]=='þ')
+		{
+			//tahta.yerlestir(new Sah(),koord,kendi_rengi,yedigi_renk,tahta);
+		}
+		else 
+		{
+			
+			
+			
+			System.out.println("Boþ karenin koordinatlarýný girdiniz!!");
+		}
+		
+	}
+	
+	
 }
