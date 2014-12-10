8b59a412f913cc13bd063df173dc0d8ff716d03f
Fatma Koç <fatmakoc94@gmail.com>
2014-12-10 23:22:42 +0200

    okey

--- src/Satranc/At.java
+++ src/Satranc/At.java
@@ -5,40 +5,49 @@ public class At extends Tas {
 	
 	public void hamleYap(Koordinat koordinat,Tahta tahta) {
 	
-		int xFark=Math.abs(koordinat.getbasX()-koordinat.getsonX());
-		int yFark=Math.abs(koordinat.getbasY()-koordinat.getsonY());
-		int Degistir=0;
+		int xFark =Math.abs(koordinat.getbasX() - koordinat.getsonX());
+		int yFark = Math.abs(koordinat.getbasY() - koordinat.getsonY());
 		
-		if(tahta.tahta[koordinat.getbasY()][koordinat.getbasX()]=='a')
-		{
-			if((xFark==2 && yFark==1) || (xFark==1 && yFark==2))
-			{
-				Degistir=1;
-			}
-			
-			
-		}
+		int Degistir = 0;
 		
-		if(tahta.tahta[koordinat.getbasY()][koordinat.getbasX()]=='A')
+		//1.oyuncunun at için oynama durumlarý
+		
+		if(tahta.tahta[koordinat.getbasY()][koordinat.getbasX()] == 'a')
 		{
-			if((xFark==2 && yFark==1) || (xFark==1 && yFark==2))
+			if(((xFark == 1 && yFark == 2) || (xFark == 2 && yFark == 1)))
 			{
-				Degistir=2;
+				//oynayacaðý koordinatýn boþ olma ve rakip taþý bulundurma durumu(yeme)
+				if((tahta.tahta[koordinat.getsonY()][koordinat.getsonX()] != ' ') || (tahta.tahta[koordinat.getsonY()][koordinat.getsonX()] == 'P') || (tahta.tahta[koordinat.getsonY()][koordinat.getsonX()] == 'K')|| (tahta.tahta[koordinat.getsonY()][koordinat.getsonX()] == 'A') || 
+						(tahta.tahta[koordinat.getsonY()][koordinat.getsonX()] == 'F')|| (tahta.tahta[koordinat.getsonY()][koordinat.getsonX()] == 'V'))
+					Degistir = 1;
 			}
 		}
 		
-		if(Degistir==1)
+		//2.oyuncunun at için oynama durumlarý
+		if(tahta.tahta[koordinat.getbasY()][koordinat.getbasX()] == 'A')
+		{
+			if(((xFark == 1 && yFark == 2) || (xFark == 2 && yFark == 1)))
+			{
+				if(tahta.tahta[koordinat.getsonY()][koordinat.getsonX()] != ' ' || tahta.tahta[koordinat.getsonY()][koordinat.getsonX()] == 'p' || tahta.tahta[koordinat.getsonY()][koordinat.getsonX()] == 'k'|| tahta.tahta[koordinat.getsonY()][koordinat.getsonX()] == 'a' || 
+						tahta.tahta[koordinat.getsonY()][koordinat.getsonX()] == 'f'|| tahta.tahta[koordinat.getsonY()][koordinat.getsonX()] == 'v')
+					Degistir = 2;
+			}
+			
+		}
+		
+		//1. ve 2.oyuncunun oynayabilme durumlarý
+		if(Degistir == 1)
 		{
 			tahta.tahta[koordinat.getsonY()][koordinat.getsonX()]='a';
-			tahta.tahta[koordinat.getbasY()][koordinat.getbasX()]=' ';
+        	tahta.tahta[koordinat.getbasY()][koordinat.getbasX()]=' ';
 		}
 		
-		else if(Degistir==2)
+		
+		else if(Degistir == 2)
 		{
 			tahta.tahta[koordinat.getsonY()][koordinat.getsonX()]='A';
-			tahta.tahta[koordinat.getbasY()][koordinat.getbasX()]=' ';
+        	tahta.tahta[koordinat.getbasY()][koordinat.getbasX()]=' ';
 		}
-		
 		
 		System.out.println();	
 		
